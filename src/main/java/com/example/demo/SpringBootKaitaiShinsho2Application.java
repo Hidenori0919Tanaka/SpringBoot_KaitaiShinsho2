1a81fbc303e8cff9a0c6cf0bd203aca01ed48d26
TNK_Hidy <hidenori091985@yahoo.co.jp>
2022-11-10 08:11:56 +0900

    first commit

---
+++ src/main/java/com/example/demo/SpringBootKaitaiShinsho2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKaitaiShinsho2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKaitaiShinsho2Application.class, args);
+	}
+
+}
